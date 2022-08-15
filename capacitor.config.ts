bc7942e9c7549471640772ee471e91f0cb8807b6
Masoud Mohajeri <masoudmohajeri.wd@gmail.com>
2022-08-15 10:14:17 +0430

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'flash-card',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
